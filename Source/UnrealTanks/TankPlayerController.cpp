84d09e1fd842d54df00dd9d79cab6483c36aa929
Ian Rapoport <iangrap@gmail.com>
2019-09-20 18:11:34 -0700

    184-virtual-and-override

--- Source/UnrealTanks/TankPlayerController.cpp
+++ Source/UnrealTanks/TankPlayerController.cpp
@@ -1,7 +1,21 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
+#include "UnrealTanks.h"
 #include "TankPlayerController.h"
+
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	auto controlledTank = GetControlledTank();
+	if (!controlledTank) 
+	{
+		UE_LOG(LogTemp, Warning, TEXT("No tank assigned to tank player controller"));
+	}
+	else 
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Tank player controller possesing %s"), *controlledTank->GetName());
+	}
+}
 
 ATank* ATankPlayerController::GetControlledTank() const 
 {
